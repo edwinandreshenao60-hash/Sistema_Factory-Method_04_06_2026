b63e4591763c86f81309374e085a273cfbca1c3c
Edwin Henao <edwinandreshenao60@gmail.com>
2026-06-04 11:15:00 -0500

    feat(proyecto): inicializar proyecto Spring Boot con dependencia Spring Web para implementacion del patron Factory Method

---
+++ src/main/java/com/sena/sistemafactory/SistemafactoryApplication.java
@@ -0,0 +1,13 @@
+package com.sena.sistemafactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemafactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemafactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/sistemafactory/SistemafactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.sistemafactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemafactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
